0bc74ff346c9e4455eab39a8b3ffed10ae7b93be
DCota <32775237+DaigaroCota@users.noreply.github.com>
2023-11-28 13:27:09 -0800

    chore: remove unused method

--- src/interfaces/IL1CrossDomainMessenger.sol
+++ src/interfaces/IL1CrossDomainMessenger.sol
@@ -2,6 +2,5 @@
 pragma solidity 0.8.18;
 
 interface IL1crossDomainMessenger {
-  function xDomainMessageSender() external returns (address);
   function sendMessage(address _target, bytes memory _message, uint32 _gasLimit) external;
 }
