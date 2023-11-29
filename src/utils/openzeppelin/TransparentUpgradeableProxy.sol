ed486bf273d019daace00da2a799e0a50b398bfa
DCota <32775237+DaigaroCota@users.noreply.github.com>
2023-11-29 15:22:31 -0800

    chore: inherit from same oz library to solve discrepancies

--- src/utils/openzeppelin/TransparentUpgradeableProxy.sol
+++ src/utils/openzeppelin/TransparentUpgradeableProxy.sol
@@ -3,26 +3,10 @@
 
 pragma solidity ^0.8.0;
 
+import {ITransparentUpgradeableProxy} from
+  "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import {IERC1967} from "@openzeppelin/contracts/interfaces/IERC1967.sol";
 import {ERC1967Proxy} from "./ERC1967Proxy.sol";
-
-/**
- * @dev Interface for {TransparentUpgradeableProxy}. In order to implement transparency, {TransparentUpgradeableProxy}
- * does not implement this interface directly, and some of its functions are implemented by an internal dispatch
- * mechanism. The compiler is unaware that these functions are implemented by {TransparentUpgradeableProxy} and will not
- * include them in the ABI so this interface must be used to interact with it.
- */
-interface ITransparentUpgradeableProxy is IERC1967 {
-  function admin() external view returns (address);
-
-  function implementation() external view returns (address);
-
-  function changeAdmin(address) external;
-
-  function upgradeTo(address) external;
-
-  function upgradeToAndCall(address, bytes memory) external payable;
-}
 
 /**
  * @dev This contract implements a proxy that is upgradeable by an admin.
