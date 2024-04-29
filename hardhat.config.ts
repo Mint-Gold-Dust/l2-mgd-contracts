fd04131407424919a5617360088909c108dc6da1
DCota <32775237+DaigaroCota@users.noreply.github.com>
2024-04-29 15:00:15 -0700

    chore: add hardhat config details to ease via-ir compiling actions

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,19 +1,32 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-foundry";
-import * as fs from 'fs';
-
-// Task to generate a wallet with a randomly created mnemonic
-task("generate", "Create a wallet for builder deploys", async (_, { ethers }) => {
-  const newWallet = ethers.Wallet.createRandom();
-  const address = newWallet.address;
-  const mnemonic = newWallet.mnemonic.phrase
-  console.log("🔐 Account Generated as " + address + " and set as mnemonic in packages/hardhat");
-  fs.writeFileSync("./" + address + ".txt", `${address}\n${mnemonic.toString()}\n${newWallet.privateKey}`);
-  fs.writeFileSync("./mnemonic.txt", mnemonic.toString());
-});
+import dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`${process.env.PKEY}`],
+      chainId: 11155111,
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`${process.env.PKEY}`],
+      chainId: 1,
+    },
+    basesepolia: {
+      url: `https://base-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_KEY_BASESEPOLIA}`,
+      accounts: [`${process.env.PKEY}`],
+      chainId: 84532,
+    },
+    base: {
+      url: `https://base-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY_BASE}`,
+      accounts: [`${process.env.PKEY}`],
+      chainId: 8543,
+    },
+  },
   solidity: {
     version: "0.8.18",
     settings: {
@@ -21,11 +34,20 @@ const config: HardhatUserConfig = {
         enabled: true,
         runs: 10000,
       },
+      viaIR: true,
       outputSelection: {
-        '*': {
-          '*': ['storageLayout'],
+        "*": {
+          "*": ["storageLayout"],
         },
       },
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: `${process.env.KEY_MAINNET}`,
+      mainnet: `${process.env.KEY_MAINNET}`,
+      base: `${process.env.KEY_BASE}`,
+      basesepolia: `${process.env.KEY_BASE}`,
     },
   },
 };
